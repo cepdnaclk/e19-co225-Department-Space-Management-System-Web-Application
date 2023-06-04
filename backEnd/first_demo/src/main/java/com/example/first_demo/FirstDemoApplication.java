f526627c81f7d722d189a6c61e90c35e649fdc65
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-06-04 07:38:38 +0530

    save additional files

--- backEnd/first_demo/src/main/java/com/example/first_demo/FirstDemoApplication.java
+++ backEnd/first_demo/src/main/java/com/example/first_demo/FirstDemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.sharedspaces_demo;
+package com.example.first_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SharedspacesDemoApplication {
+public class FirstDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SharedspacesDemoApplication.class, args);
+		SpringApplication.run(FirstDemoApplication.class, args);
 	}
 
 }
