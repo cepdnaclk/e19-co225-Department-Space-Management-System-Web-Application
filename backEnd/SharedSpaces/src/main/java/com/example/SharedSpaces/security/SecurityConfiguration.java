5359389d8098e1acdcafcef68b1120b24b551161
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-06-08 02:24:05 +0530

    save

--- backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/security/SecurityConfiguration.java
+++ backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/security/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                                 .requestMatchers(
-                                        "/**"
+                                        "/log"
                                 ).permitAll()
                                 .anyRequest().authenticated()
                 )
