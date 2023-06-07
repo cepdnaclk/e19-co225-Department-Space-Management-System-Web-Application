587d4ae357c0856c5ab8bb8f0b87367d6a153931
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-06-07 15:07:44 +0530

    save project

---
+++ backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/SharedSpacesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SharedSpaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedSpacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedSpacesApplication.class, args);
+	}
+
+}
