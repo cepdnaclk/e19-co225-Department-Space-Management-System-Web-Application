2af83914606963ff2c5b251d20e7448192edd12f
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-06-07 23:56:55 +0530

    adding security

---
+++ backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/security/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.example.SharedSpaces.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    // get the jwtfilter
+//    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers(
+                                        "/log",
+                                        "/**"
+                                ).permitAll()
+                                .anyRequest().authenticated()
+
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+
+        ;
+
+        return http.build();
+    }
+
+}
