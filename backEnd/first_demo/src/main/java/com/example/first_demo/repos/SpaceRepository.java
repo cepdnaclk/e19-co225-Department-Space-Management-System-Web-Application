b9ede374ee170603ef2023be3c6f06df066955b4
Mansitha Eashwara <115540664+mansithaeashwara@users.noreply.github.com>
2023-06-08 23:53:53 +0530

    added space repo
    added space repo

---
+++ backEnd/first_demo/src/main/java/com/example/first_demo/repos/SpaceRepository.java
@@ -0,0 +1,12 @@
+package com.example.first_demo.repos;
+
+
+
+import com.example.first_demo.models.Space;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpaceRepository extends JpaRepository<Space, Integer> {
+
+}
