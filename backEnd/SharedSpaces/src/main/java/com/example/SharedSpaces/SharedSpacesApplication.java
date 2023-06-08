371cf3371591df41a6d34e840ed845a2fb79dba0
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-06-08 07:51:04 +0530

    login verification

--- backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/SharedSpacesApplication.java
+++ backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/SharedSpacesApplication.java
@@ -1,5 +1,6 @@
 package com.example.SharedSpaces;
 
+import com.example.SharedSpaces.service.LogService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,6 @@ public class SharedSpacesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SharedSpacesApplication.class, args);
 	}
+
 
 }
--- backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/service/LogService.java
+++ backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/service/LogService.java
@@ -3,11 +3,23 @@ package com.example.SharedSpaces.service;
 import com.example.SharedSpaces.models.User;
 import org.springframework.stereotype.Service;
 
+import java.util.Base64;
+
 @Service
 public class LogService {
 
-    public User checkToken(String token) {
-        return new User("Kanishka", "Student");
+    public User extractClaims(String token) {
+
+        String payLoadToken = token.split("\\.")[1];
+
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+
+        String payLoad = new String(decoder.decode(payLoadToken));
+
+        System.out.println(payLoad);
+
+        return new User("name", "email");
+
     }
 
 }
