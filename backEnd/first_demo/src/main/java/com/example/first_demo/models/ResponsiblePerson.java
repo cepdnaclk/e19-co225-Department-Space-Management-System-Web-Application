5306fe69db57b1cf927c3799f01675195e3317f5
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-06-09 10:41:38 +0530

    save

--- backEnd/first_demo/src/main/java/com/example/first_demo/models/ResponsiblePerson.java
+++
@@ -1,8 +0,0 @@
-package com.example.first_demo.models;
-
-public class ResponsiblePerson extends  Person{
-
-    public ResponsiblePerson(String firstName, String lastName, String email) {
-        super(firstName, lastName, email);
-    }
-}
