ec07f54871c789dc7675ea05409360757bcc5f3b
kaushithamsilva <kaushithamsilva@gmail.com>
2023-06-12 22:54:30 +0530

    Added Time Column. + Reservations resize according ro time.

--- sharedspaces-react/src/components/Calendar.js
+++ sharedspaces-react/src/components/Calendar.js
@@ -12,9 +12,9 @@ const Calendar = ({ selectSpace, spaceReservations }) => {
     dateList.push(date);
   }
 
-  //get the start time and end time
+  //get the start time and end time and populate an array for each hour interval
   const startTime = 8;
-  const endTime = 17;
+  const endTime = 18;
   const hourIntervals = Array.from(
     { length: endTime - startTime },
     (_, index) => index + startTime
@@ -34,7 +34,7 @@ const Calendar = ({ selectSpace, spaceReservations }) => {
           )}
         />
       ))}
-      <div></div>
+      <TimeColumn hours={hourIntervals} />
     </div>
   );
 };
@@ -56,7 +56,6 @@ const Day = ({ dateObj, hourIntervals, dayReservations }) => {
       {hourIntervals.map((hour) => (
         <Slot
           key={`${dateObj.getDate()}-${hour}`}
-          hour={hour}
           slotReservations={dayReservations.filter(
             (reservation) => Math.floor(reservation.startTime / 100) === hour
           )}
@@ -66,23 +65,53 @@ const Day = ({ dateObj, hourIntervals, dayReservations }) => {
   );
 };
 
-const Slot = ({ hour, slotReservations }) => {
+const Slot = ({ slotReservations }) => {
   return (
     //TODO: Add Tab Navigation -- Conflict of erronous clicks
     <div className={styles.slot}>
-      {slotReservations.map((reservation) => (
-        <button
-          key={reservation.startTime}
-          className={styles.reservation}
-          style={{ height: reservation.startTime - reservation.endTime }}
-        >
-          {reservation.title}
-        </button>
-      ))}
+      {slotReservations.map((reservation) => {
+        const minutes =
+          (Math.floor(reservation.endTime / 100) -
+            Math.floor(reservation.startTime / 100)) *
+            60 +
+          ((reservation.endTime % 100) - (reservation.startTime % 100));
+        console.log(reservation.startTime);
+        return (
+          <button
+            key={reservation.startTime}
+            className={styles.reservation}
+            style={{
+              height: `${(minutes / 60) * 100}%`,
+              top: `${((reservation.startTime % 100) / 60) * 100}%`,
+            }}
+          >
+            {reservation.title}
+          </button>
+        );
+      })}
     </div>
   );
 };
 
+const TimeColumn = ({ hours }) => {
+  const hourStrings = hours.map((hour) => {
+    return new Intl.DateTimeFormat("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+    }).format(new Date(`2000-01-01T${hour.toString().padStart(2, "0")}:00`));
+  });
+
+  return (
+    <div className={styles.timeCol}>
+      <div className={styles.controller}></div>
+      {hourStrings.map((hour) => (
+        <div className={styles.hour} key={hour}>
+          {hour}
+        </div>
+      ))}
+    </div>
+  );
+};
 // const getHourlyIncrements = (start, end) => {
 //   const startDate = new Date(`2000-01-01T${start}:00`);
 //   const endDate = new Date(`2000-01-01T${end}:00`);
