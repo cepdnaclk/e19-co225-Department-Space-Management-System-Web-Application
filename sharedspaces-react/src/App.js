c906abb1983586a04e9140d9e5b6a46ae35c5537
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-06-13 07:56:51 +0530

    fixed roauting

--- sharedspaces-react/src/App.js
+++ sharedspaces-react/src/App.js
@@ -7,9 +7,7 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <Router>
-        <AppRouter/>
-      </Router>
+      <AppRouter />
     </>
   );
 };
--- sharedspaces-react/src/index.js
+++ sharedspaces-react/src/index.js
@@ -4,16 +4,17 @@ import "./styles/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import {GoogleOAuthProvider} from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { BrowserRouter } from "react-router-dom";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="461418541066-5c9p2cf0d6d8qthhgh7n2tjrd28pf3t9.apps.googleusercontent.com">
-      <App/>
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId="461418541066-5c9p2cf0d6d8qthhgh7n2tjrd28pf3t9.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
