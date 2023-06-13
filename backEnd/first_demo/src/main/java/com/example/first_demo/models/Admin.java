f78ad16da5a1f45acccfc1325e59da6b9021e7eb
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-06-13 20:07:28 +0530

    save

--- backEnd/first_demo/src/main/java/com/example/first_demo/models/Admin.java
+++
@@ -1,9 +0,0 @@
-package com.example.first_demo.models;
-
-public class Admin extends Person {
-
-    public Admin(String firstName, String lastName, String email) {
-        super(firstName, lastName, email);
-    }
-
-}
