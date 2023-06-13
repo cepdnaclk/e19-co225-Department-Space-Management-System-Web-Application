7f2e66494225d1eb5cf5c2b5b62b30f82e56d8b7
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-06-13 21:15:16 +0530

    save

--- backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/controller/WaitingController.java
+++ backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/controller/WaitingController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,7 +23,7 @@ public class WaitingController {
 
     @GetMapping
     public List<WaitingResponse> getWaitingList(){
-
+        return new ArrayList<>();
     }
 
 }
