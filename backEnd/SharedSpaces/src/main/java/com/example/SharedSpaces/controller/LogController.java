721c77f4ddcf2262b4efe9115f5cf614bcd69d8a
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2023-06-14 23:49:28 +0530

    profile  created

--- backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/controller/LogController.java
+++ backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/controller/LogController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000" )
+@CrossOrigin
 @RestController
 @RequestMapping("log")
 public class LogController {
