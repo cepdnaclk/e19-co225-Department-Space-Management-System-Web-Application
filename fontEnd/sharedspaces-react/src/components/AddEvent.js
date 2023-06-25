58ac8e9dbddf892684d84ad599fa7ac7167f5014
kaushithamsilva <kaushithamsilva@gmail.com>
2023-06-25 18:06:29 +0530

    Completed the react-select component for the responsible person selection

--- fontEnd/sharedspaces-react/src/components/AddEvent.js
+++ fontEnd/sharedspaces-react/src/components/AddEvent.js
@@ -3,40 +3,47 @@ import { FiMapPin, FiCheck } from "react-icons/fi";
 import { LuCalendarDays } from "react-icons/lu";
 import { FaRegClock } from "react-icons/fa";
 import { spaces } from "../data";
-import { getDateInFormat } from "../utils";
+import { getDateInFormat, getTimeString } from "../utils";
 import Select from "react-select";
 import classNames from "classnames";
-// const customStyles = {
-//   control: (provided, state) => ({
-//     ...provided,
-//     marginTop: "0.5em",
-//     fontSize: "0.9em",
-//   }),
-//   option: (provided, state) => ({
-//     ...provided,
-//     fontSize: "0.9em",
-//   }),
-// };
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
+import { useEffect, useState } from "react";
+
+const groupedOptions = [
+  {
+    label: "Lecturers",
+    options: [
+      { value: "john", label: "John" },
+      { value: "sarah", label: "Sarah" },
+      { value: "peter", label: "Peter" },
+    ],
+  },
+  {
+    label: "Instructors",
+    options: [
+      { value: "bob", label: "Bob" },
+      { value: "alice", label: "Alice" },
+      { value: "jane", label: "Jane" },
+    ],
+  },
 ];
 
-const ResponsibleSelect = () => (
-  <Select
-    options={options}
-    classNames={{
-      control: (state) =>
-        classNames(
-          styles.selectControl,
-          state.isFocused && styles.selectControlFocused
-        ),
-      option: (state) => classNames(styles.selectOption),
-    }}
-  />
-);
 const AddEvent = ({ hour, spaceId, date }) => {
+  const [startTime, setStartTime] = useState(getTimeString(hour * 100));
+  const [endTime, setEndTime] = useState(getTimeString(hour * 100 + 40));
+
+  useEffect(() => {
+    setStartTime(getTimeString(hour * 100));
+    setEndTime(getTimeString(hour * 100 + 40));
+  }, [hour]);
+
+  const handleStartTimeChange = (event) => {
+    setStartTime(event.target.value);
+  };
+
+  const handleEndTimeChange = (event) => {
+    setEndTime(event.target.value);
+  };
+
   const spaceName = spaces.find((s) => s.id === spaceId).name;
   return (
     <div className={styles.addEvent}>
@@ -45,6 +52,7 @@ const AddEvent = ({ hour, spaceId, date }) => {
           type="text"
           placeholder="Add Title"
           className={styles.inputTitle}
+          maxLength={25}
         />
         <div className={styles.info}>
           <p className={styles.infoItem}>
@@ -59,19 +67,21 @@ const AddEvent = ({ hour, spaceId, date }) => {
             <FaRegClock />
             <input
               type="text"
-              placeholder="10:00AM"
+              value={startTime}
+              onChange={handleStartTimeChange}
               className={styles.time}
             />{" "}
             -
-            <input type="text" placeholder="10:40AM" className={styles.time} />
+            <input
+              type="text"
+              value={endTime}
+              onChange={handleEndTimeChange}
+              className={styles.time}
+            />
           </p>
         </div>
         <p className={styles.pResPerson}>Responsible Person</p>
-        {/* <input
-          type="text"
-          placeholder="Add a lecturer or an instructor"
-          className={styles.inputResPerson}
-        /> */}
+
         <ResponsibleSelect />
         <button type="submit" className={styles.confirmBtn}>
           <FiCheck className={styles.checkIcon} />
@@ -83,3 +93,27 @@ const AddEvent = ({ hour, spaceId, date }) => {
 };
 
 export default AddEvent;
+
+const ResponsibleSelect = () => (
+  <Select
+    placeholder="Select a reponsible person"
+    options={groupedOptions}
+    classNames={{
+      container: () => styles.selectContainer,
+      control: (state) =>
+        classNames(
+          styles.selectControl,
+          state.isFocused && styles.selectControlFocused
+        ),
+      option: (state) => classNames(styles.selectOption),
+      placeholder: (state) => classNames(styles.selectPlaceholder),
+      input: (state) =>
+        classNames(
+          styles.selectInput,
+          state.isFocused && styles.selectInputFocused
+        ),
+      menu: (state) => classNames(styles.selectMenu),
+      valueContainer: (state) => styles.selectValueContainer,
+    }}
+  />
+);
