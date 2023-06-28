6169530fdb87f9a734097a8dd00208470285cb94
Mansitha Eashwara <115540664+mansithaeashwara@users.noreply.github.com>
2023-06-28 17:34:11 +0530

    Repos Recheck

--- backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/repos/AdminRepository.java
+++ backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/repos/AdminRepository.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
+// This interface provides CRUD operations for the Admin entity.
 public interface AdminRepository extends CrudRepository<Admin, Long> {
+
+    // Finds an Admin entity by email.
     Optional<Admin> findByEmail(String email);
 }
--- backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/repos/UserRepository.java
+++ backEnd/SharedSpaces/src/main/java/com/example/SharedSpaces/repos/UserRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
+// This interface provides CRUD operations for the User entity.
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    // Finds a User entity by email.
     Optional<User> findByEmail(String email);
 }
