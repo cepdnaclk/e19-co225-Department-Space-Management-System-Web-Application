2c3e0e3b1103bd4ba18dcbced7f249a12f3fdaec
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2023-06-29 13:15:56 +0530

    new event adding

--- fontEnd/sharedspaces-react/src/components/AddEvent.js
+++ fontEnd/sharedspaces-react/src/components/AddEvent.js
@@ -3,7 +3,7 @@ import { FiMapPin, FiCheck } from "react-icons/fi";
 import { LuCalendarDays } from "react-icons/lu";
 import { FaRegClock, FaPlus } from "react-icons/fa";
 import { spaces } from "../data";
-import { getDateInFormat, getTimeString, setTimeFormat } from "../utils";
+import { getDateInFormat, getTimeString, setTimeFormat, mapTimeStringToInteger} from "../utils";
 import Select from "react-select";
 import classNames from "classnames";
 import { useEffect, useState } from "react";
@@ -73,13 +73,15 @@ const AddEvent = ({
 
   const handleStartTimeChange = (event) => {
     setStartTime(event.target.value);
-    if(event.target.value)
-    validateReservation(reservations);
+    if(mapTimeStringToInteger(event.target.value))
+    {validateReservation(reservations);}
   };
 
   const handleEndTimeChange = (event) => {
     setEndTime(event.target.value);
-    validateReservation(reservations);
+    if(mapTimeStringToInteger(event.target.value))
+    {validateReservation(reservations);
+    }
   };
 
   const validateReservation = (reservationList) => {
