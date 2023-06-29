b007dcdc85d9470ea8d79a25c0d7361225a2ffad
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2023-06-29 14:38:08 +0530

    space reservations changed

--- fontEnd/sharedspaces-react/src/components/AddEvent.js
+++ fontEnd/sharedspaces-react/src/components/AddEvent.js
@@ -24,13 +24,13 @@ const AddEvent = ({
   endTimeProp,
   spaceId,
   date,
-  reservationList,
+  spaceReservations
 }) => {
   const [startTime, setStartTime] = useState(getTimeString(startTimeProp));
   const [endTime, setEndTime] = useState(getTimeString(endTimeProp));
   const [responsible, setResponsible] = useState([]);
   const [reservations, setReservations] = useState([]);
-  const [isClash, setClash] = useState(true);
+  const [isClash, setClash] = useState(false);
   const spaceName = spaces.find((s) => s.id === spaceId).name;
 
   function mapResponsible() {
@@ -88,7 +88,6 @@ const AddEvent = ({
   };
 
   const validateReservation = (reservationList) => {
-    const currentTime = new Date.getTime();
     const startTimeFormatted = setTimeFormat(startTime);
     const endTimeFormatted = setTimeFormat(endTime);
 
@@ -103,7 +102,7 @@ const AddEvent = ({
     getReservation();
 
     for (let i = 0; i < reservations.length; i++) {
-      if ((reservationList[i].date = date)) {
+      if ((reservations[i].filter)) {
         if (
           (reservations[i].startTime > startTimeFormatted &&
             reservations[i].startTime < endTimeFormatted) ||
@@ -143,14 +142,14 @@ const AddEvent = ({
             <input
               type="text"
               value={startTime}
-              onChange={[handleStartTimeChange]}
+              onChange={handleStartTimeChange}
               className={styles.time}
             />{" "}
             -
             <input
               type="text"
               value={endTime}
-              onChange={[handleEndTimeChange]}
+              onChange={handleEndTimeChange}
               className={styles.time}
             />
           </p>
