b9032a1ac4e884822f317257442f634ba9f46406
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-06-29 11:48:22 +0530

    db initializing

---
+++ backEnd/db.sql
@@ -0,0 +1,46 @@
+INSERT INTO user
+VALUES(
+        1,
+        "e19129@eng.pdn.ac.lk",
+        "Kanishka",
+        "Gunawardana"
+    ),
+    (
+        2,
+        "e19408@eng.pdn.ac.lk",
+        "Sanduni",
+        "Ubayasiri"
+    ),
+    (3, "e19372@eng.pdn.ac.lk", "Kaushitha", "Silva"),
+    (4, "e19074@eng.pdn.ac.lk", "Mansitha", "Eshwara"),
+    (
+        5,
+        "e19009@eng.pdn.ac.lk",
+        "Chandula",
+        "Adhikari"
+    );
+INSERT INTO responsible_person
+VALUES ("Dr.", 1),
+    ("Prof.", 2);
+INSERT INTO admin
+VALUES (3),
+    (4);
+INSERT INTO spaces
+VALUES (1, 100, "1st floor", "AV,TV", "IMAGE", "LAB-01"),
+    (2, 50, "5st floor", "AV", "IMAGE", "LAB- 02"),
+    (
+        3,
+        100,
+        "3st floor",
+        "AV,TV",
+        "IMAGE",
+        "Network LAB"
+    ),
+    (
+        4,
+        75,
+        "2st floor",
+        "",
+        "IMAGE",
+        "Discussion Room"
+    );
