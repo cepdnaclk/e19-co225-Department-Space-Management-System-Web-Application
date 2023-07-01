690d75626973c31de814c988c2dfd2eecf1f615d
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-07-01 23:24:37 +0530

    responsible reservation

--- fontEnd/sharedspaces-react/src/components/manageReservations/MyReservations.js
+++ fontEnd/sharedspaces-react/src/components/manageReservations/MyReservations.js
@@ -15,6 +15,8 @@ const MyReservations = () => {
   const [pastReservations, setPastReservations] = useState([]);
   const [currentReservations, setCurrentReservations] = useState([]);
   const [responsibleReservations, setResponsibleReservations] = useState([]);
+  const [responsibleCurrentReservations, setResponsibleCurrentReservations] =
+    useState([]);
 
   const getReservation = () => {
     getUserReservations(setReservations, user.email);
@@ -53,6 +55,19 @@ const MyReservations = () => {
     }
   }, [reservations]);
 
+  useEffect(() => {
+    if (reservations !== []) {
+      setResponsibleCurrentReservations(
+        responsibleReservations.filter((res) => {
+          const date = new Date(res.date);
+          const currentDate = new Date();
+          if (date >= currentDate) return true;
+          return false;
+        })
+      );
+    }
+  }, [responsibleReservations]);
+
   return (
     <div className={styles.container}>
       <h2>Upcoming Reservations</h2>
@@ -84,7 +99,7 @@ const MyReservations = () => {
           <>
             <h2 className={styles.pastReservations}>Responsible</h2>
             <ReservationTable
-              reservations={responsibleReservations}
+              reservations={responsibleCurrentReservations}
               user={user}
               waitingList={false}
               isActionable={true}
